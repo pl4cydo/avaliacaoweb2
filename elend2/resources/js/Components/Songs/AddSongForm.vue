70ce4207f320225696c77bd7aeb7422833a03503
Arthur Alves <arthuralves292004@gmail.com>
2023-07-06 15:50:58 -0300

    finish

---
+++ elend2/resources/js/Components/Songs/AddSongForm.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { useForm } from '@inertiajs/vue3';
+import MyTextInput from '../MyTextInput.vue';
+import PrimaryButton from '../PrimaryButton.vue';
+
+const emit = defineEmits(['song-added']);
+
+const form = useForm({
+    title: '',
+    artist: '',
+    album: '',
+    genre: '',
+});
+
+function submit() {
+    form.post(route('songs.store'), {
+        onSuccess: () => {
+            form.reset();
+            emit('song-added');
+        },
+    });
+}
+</script>
+
+<template>
+    <form @submit.prevent="submit">
+        <fieldset class="border shadow-md p-2 rounded-md w-full m-auto">
+            <legend class="font-bold">
+                Add new Song
+            </legend>
+            <div>
+                <MyTextInput v-model="form.title" label="Title" autofocus />
+                <MyTextInput v-model="form.artist" label="Artist" />
+                <MyTextInput v-model="form.album" label="Album" />
+                <MyTextInput v-model="form.genre" label="Genre" />
+                <PrimaryButton class="">Save</PrimaryButton>
+            </div>
+        </fieldset>
+    </form>
+</template>
